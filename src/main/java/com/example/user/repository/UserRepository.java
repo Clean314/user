efa61a11b109918b8b20b32640d7ebe74e7eb3d2
Clean314 <pipie314@protonmail.com>
2023-04-06 19:41:54 +0900

    불필요한 타입 반환

--- src/main/java/com/example/user/repository/UserRepository.java
+++ src/main/java/com/example/user/repository/UserRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
     Optional<User> findUserByUserId(String userId);
-    Optional<User> deleteUserByUserId(String userId);
+    void deleteUserByUserId(String userId);
 }
